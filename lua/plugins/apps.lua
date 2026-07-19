6004ea476242764f5f438efc29347840a39eccea
siriuszx <114768817+Siriuszx@users.noreply.github.com>
2026-07-19 17:22:35 +0200

    Update add leetcode plugin

--- lua/plugins/apps.lua
+++ lua/plugins/apps.lua
@@ -1,10 +1,22 @@
 return {
   {
-    -- {
-    --   "nvzone/typr",
-    --   dependencies = "nvzone/volt",
-    --   opts = {},
-    --   cmd = { "Typr", "TyprStats" },
-    -- },
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    cmd = "Leet",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    keys = {
+      { "<leader>al", "<cmd>Leet<cr>", desc = "LeetCode" },
+    },
+    opts = {
+      plugins = {
+        non_standalone = true,
+      },
+      picker = {
+        provider = "snacks-picker",
+      },
+    },
   },
 }
